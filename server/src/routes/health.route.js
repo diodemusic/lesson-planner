a23f8ea56ca58d90493ab5b57823e268bd6b4af1
diodemusic <diodemusicuk@gmail.com>
2026-06-25 21:15:38 +0100

    Initial commit

--- server/src/routes/health.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { health } = require("../controllers/health.controller");
-
-router.get("/", health);
-
-module.exports = router;
--- server/src/routes/module.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getModules, postModule, deleteModule } = require("../controllers/module.controller");
-
-router.get("/", getModules);
-router.post("/", postModule);
-router.delete("/:id", deleteModule);
-
-module.exports = router;
